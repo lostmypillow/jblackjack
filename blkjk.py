ae4d1d0f35c7064d868f08abe166fd5643fdc643
Johnny L <jmlin0101@gmail.com>
2023-09-05 20:28:34 +0800

    09052023

---
+++ blkjk.py
@@ -0,0 +1,15 @@
+deck_o_cards = {
+    "A of ♣"    "A of ♥"    "A of ♦"    "A of ♠"    
+"2 of ♣"    "2 of ♥"    "2 of ♦"    "2 of ♠"    
+"3 of ♣"    "3 of ♥"    "3 of ♦"    "3 of ♠"    
+"4 of ♣"    "4 of ♥"    "4 of ♦"    "4 of ♠"    
+"5 of ♣"    "5 of ♥"    "5 of ♦"    "5 of ♠"    
+"6 of ♣"    "6 of ♥"    "6 of ♦"    "6 of ♠"    
+"7 of ♣"    "7 of ♥"    "7 of ♦"    "7 of ♠"    
+"8 of ♣"    "8 of ♥"    "8 of ♦"    "8 of ♠"    
+"9 of ♣"    "9 of ♥"    "9 of ♦"    "9 of ♠"    
+"10 of ♣"   "10 of ♥"   "10 of ♦"   "10 of ♠"   
+"J of ♣"    "J of ♥"    "J of ♦"    "J of ♠"    
+"Q of ♣"    "Q of ♥"    "Q of ♦"    "Q of ♠"    
+"K of ♣"    "K of ♥"    "K of ♦"    "K of ♠" 
+}
